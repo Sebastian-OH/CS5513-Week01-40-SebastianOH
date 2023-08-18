74f18aedc3079beb7a37a802aaf5622dd0752297
Sebastian-OH <121125522+Sebastian-OH@users.noreply.github.com>
2023-08-18 01:31:33 +0000

    Changes Something

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ let myhttp = require("http");
 let myServer = myhttp.createServer(
   function(myRequest, myResponse) {
     console.log(myRequest.url);
-
+    let secretText;
+    let secretHeading;
+    if (myRequest.url === "/secret") {
+      secretHeading = "<h1>Enjoy the Treasure</h1>"
+      secretText = "<p>the treasure is located at 33.8111° N, 117.9220° W</p>";
+    }
+    else {
+      secretHeading = "<h1>Welcome to this NORMAL PAGE</h1>"
+      secretText = "<p>Nothing secret here, I promise</p>";
+    }
     myResponse.writeHead(200, {"Content-Type": "text/plain"});
-    myResponse.end("Hello World!");
+    myResponse.write(secretHeading);
+    myResponse.write(secretText);
+    myResponse.end();
   }
 );
 
